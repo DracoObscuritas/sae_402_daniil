dca7639808cda8f6b6079f427aa0f06e5644bc24
DracoObscuritas <dposlavskiy@yahoo.fr>
2025-04-26 20:06:38 +0200

    le scroll
    Résoudre le problème de scroll

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Empêche le scroll lors du toucher ou de l'appui sur espace
+window.addEventListener('touchstart', (e) => {
+    e.preventDefault();
+}, { passive: false});
+window.addEventListener('touchmove', (e) => {
+    e.preventDefault();
+}, { passive: false });
+window.addEventListener('keydown', (e) => {
+    if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(e.code)) {
+        e.preventDefault();
+    }
+});
+
 // Récupération du canvas et du contexte de dessin 2D
 const canvas = document.getElementById('jeuCanvas');
 const ctx = canvas.getContext("2d");
